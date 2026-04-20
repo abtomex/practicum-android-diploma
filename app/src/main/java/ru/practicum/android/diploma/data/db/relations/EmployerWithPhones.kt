49c3e8c2069c0ee64c50149b85c1612c725a122b
Dmitry T <teresch27@gmail.com>
2026-04-20 23:49:44 +0300

    Микрофикс (unused import)

--- app/src/main/java/ru/practicum/android/diploma/data/db/relations/EmployerWithPhones.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/relations/EmployerWithPhones.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.data.db.relations
 
 import androidx.room.Embedded
-import androidx.room.Entity
 import androidx.room.Relation
 import ru.practicum.android.diploma.data.db.entity.EmployerEntity
 import ru.practicum.android.diploma.data.db.entity.PhoneEntity
