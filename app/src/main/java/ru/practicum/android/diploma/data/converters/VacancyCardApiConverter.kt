75e63e5ab68e82de3f98221cbc90835d6a874d77
abtomex <34104331+abtomex@users.noreply.github.com>
2026-04-22 08:55:14 +0300

    Ревью пулл-реквеста Вспомогательные классы и сущности (#27)
    * Репозитории, конвертеры, дополнительные классы data слоя
    
    * Устранение замечаний detect
    
    * Добавил репозитории и конвертеры в DI

---
+++ app/src/main/java/ru/practicum/android/diploma/data/converters/VacancyCardApiConverter.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma.data.converters
+
+import ru.practicum.android.diploma.data.dto.vacancies.VacancyCardDto
+import ru.practicum.android.diploma.domain.models.VacancyCard
+
+class VacancyCardApiConverter : ApiConverter<VacancyCardDto, VacancyCard> {
+    override fun map(dto: VacancyCardDto): VacancyCard {
+        return VacancyCard(
+            id = dto.id,
+            name = dto.name,
+            company = dto.company,
+            city = dto.city,
+            salary = dto.salary,
+            logo = dto.logo
+        )
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/domain/IndustriesRepository.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.domain
+
+import ru.practicum.android.diploma.domain.models.Industry
+
+interface IndustriesRepository : ApiRepository<Industry>
