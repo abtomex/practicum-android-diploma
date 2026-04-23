3dce350d404fb04ae05122272135d595cd6b25e2
Dmitry T <teresch27@gmail.com>
2026-04-24 02:54:13 +0300

    Методы репозитория для работы с избранными вакансиями (декларация)

--- app/src/main/java/ru/practicum/android/diploma/data/VacanciesRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/VacanciesRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package ru.practicum.android.diploma.data
 
+import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.data.converters.VacancyCardApiConverter
 import ru.practicum.android.diploma.data.dto.vacancies.VacanciesRequestDto
 import ru.practicum.android.diploma.data.dto.vacancies.VacancyResponseDto
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.domain.VacanciesRepository
 import ru.practicum.android.diploma.domain.models.VacancyCard
+import ru.practicum.android.diploma.domain.models.VacancyDetails
 
 class VacanciesRepositoryImpl(
     val networkClient: NetworkClient,
@@ -15,6 +17,22 @@ class VacanciesRepositoryImpl(
     override suspend fun getAllFromApi(): List<VacancyCard> {
         return (networkClient.doRequest(VacanciesRequestDto()) as VacancyResponseDto)
             .items.map { vacancyCardDto -> apiConverter.map(vacancyCardDto) }
+    }
+
+    override fun getAllVacancyCards(): Flow<List<VacancyCard>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getVacancyDetails(vacancyId: String): Flow<VacancyDetails> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addVacancyToFavorites(vacancy: VacancyDetails) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeVacancyFromFavorites(vacancyId: String) {
+        TODO("Not yet implemented")
     }
 
 }
