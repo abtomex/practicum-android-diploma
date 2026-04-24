0dbf1b9abdd6933f921cb209a726b074fd88f282
dmitriy <dchechumaev@mail.ru>
2026-04-25 02:07:38 +0300

    Интеграционные юнит-тесты на тестирование успешной интеграции с АПИ

--- app/src/main/java/ru/practicum/android/diploma/data/AreasRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/AreasRepositoryImpl.kt
@@ -1,8 +1,8 @@
 package ru.practicum.android.diploma.data
 
 import ru.practicum.android.diploma.data.converters.AreasApiConverter
+import ru.practicum.android.diploma.data.dto.Response
 import ru.practicum.android.diploma.data.dto.areas.AreasRequestDto
-import ru.practicum.android.diploma.data.dto.areas.AreasResponseDto
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.domain.AreasRepository
 import ru.practicum.android.diploma.domain.models.Area
@@ -12,8 +12,8 @@ class AreasRepositoryImpl(
     val apiConverter: AreasApiConverter
 ) : AreasRepository {
 
-    override suspend fun getAllFromApi(): List<Area> {
-        return (networkClient.doRequest(AreasRequestDto()) as AreasResponseDto)
-            .areas.map { areaDto -> apiConverter.map(areaDto) }
+    override suspend fun getAllFromApi(): List<Area>? {
+        return (networkClient.doRequest(AreasRequestDto()) as Response.AreasResponse)
+            .body?.map { areaDto -> apiConverter.map(areaDto) }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/data/network/NetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/NetworkClient.kt
@@ -2,9 +2,7 @@ package ru.practicum.android.diploma.data.network
 
 import ru.practicum.android.diploma.data.dto.Request
 import ru.practicum.android.diploma.data.dto.Response
-import ru.practicum.android.diploma.data.dto.areas.AreasRequestDto
 
 interface NetworkClient {
-    suspend fun doRequest(dto: Request): Response
-    suspend fun doRequestList(dto: AreasRequestDto): Response
+    suspend fun doRequest(dto: Request): Response<out Any>
 }
