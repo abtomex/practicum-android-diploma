df24d5eb9493382b75696c0064f12f57f2345808
dmitriy <dchechumaev@mail.ru>
2026-04-24 23:24:56 +0300

    Приделана авторизация на Retrofit

---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/AuthInterceptor.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.data.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val token: String) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        val authenticatedRequest = originalRequest.newBuilder()
+            .header("Authorization", "Bearer $token")
+            .header("Content-Type", "application/json")
+            .build()
+
+        return chain.proceed(authenticatedRequest)
+    }
+}
--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,18 +1,31 @@
 package ru.practicum.android.diploma.di
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.BuildConfig
+import ru.practicum.android.diploma.data.network.AuthInterceptor
 import ru.practicum.android.diploma.data.network.EndpointsApiService
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 
 val dataModule = module {
 
+    single<OkHttpClient> {
+        val token: String = BuildConfig.API_ACCESS_TOKEN
+        OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(token))
+            .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+            .build()
+    }
+
     single<EndpointsApiService> {
         Retrofit.Builder()
-            .baseUrl("https://android-diploma.education-services.ru")
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(get())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(EndpointsApiService::class.java)
