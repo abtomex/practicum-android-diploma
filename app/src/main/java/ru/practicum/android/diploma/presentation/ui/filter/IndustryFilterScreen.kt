8f8e0406da6f6eee87de1d23319a15e501bb886f
Dmitry T <teresch27@gmail.com>
2026-05-03 01:24:06 +0300

    Unused import

--- app/src/main/java/ru/practicum/android/diploma/presentation/ui/filter/IndustryFilterScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/ui/filter/IndustryFilterScreen.kt
@@ -19,8 +19,6 @@ import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
